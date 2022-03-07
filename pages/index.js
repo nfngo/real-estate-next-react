5c4d59ec3dcc57e6625a19c892b54dcc35186d38
Nuno Oliveira <nfnoliveira@hotmail.com>
2022-03-07 15:36:54 +0000

    Fix home page bug, add layout and property documents, add image assets

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
+import Property from "../components/Property";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 
 const Banner = ({
@@ -66,9 +67,11 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      {propertiesForSale.map((property) => (
-        <Property property={property} key={property.id} />
-      ))}
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
     </Box>
   );
 }
